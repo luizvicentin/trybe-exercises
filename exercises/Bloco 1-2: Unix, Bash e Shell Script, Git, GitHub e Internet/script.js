903d1e4ffc9cfaa9584a9c2e67f428975dc20930
Luiz Vicentin <luizpavanello@gmail.com>
2020-11-17 11:02:07 -0300

    Update script.js

--- exercises/Bloco 1-2: Unix, Bash e Shell Script, Git, GitHub e Internet/script.js
+++ exercises/Bloco 1-2: Unix, Bash e Shell Script, Git, GitHub e Internet/script.js
@@ -1,5 +1,6 @@
 
-"Eu sou Tryber e..." "Vou ter muito sucesso na programação!"
+"Eu sou Tryber e a experiência é incrível!" 
+"Com muita garra, luta, dedicação e persistência éu vou ter muito sucesso na programação!"
 
 let student = "Luiz";
 let studentSkills = 10;
