276661769490b3c5d6b29349008e047a52d04342
Luiz Vicentin <luizpavanello@gmail.com>
2020-11-25 19:48:35 -0300

    Exercicio 4 terminado

---
+++ exercises/Block 4: Introduction to JavaScript and Programming Logic/Day_01/exercises_09.js
@@ -0,0 +1,10 @@
+let number1 = 5;
+let number2 = 6;
+let number3 = 7;
+let isOdd = false;
+
+if (number1 % 2 != 0 || number2 % 2 != 0 || number3 % 2 != 0) {
+  isOdd = true;
+} 
+
+console.log(isOdd);
