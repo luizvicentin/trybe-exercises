3cb1ef082a8201bd26f7eb2103d0b09a80ce0384
Luiz Vicentin <luizpavanello@gmail.com>
2020-11-25 18:41:19 -0300

    Exercicio 3 terminado

--- exercises/Block 4: Introduction to JavaScript and Programming Logic/Day_01/exercise_03.js
+++ exercises/Block 4: Introduction to JavaScript and Programming Logic/Day_01/exercise_03.js
@@ -1,11 +1,11 @@
-console.log(greaterThan (9, 11, 20));
+console.log(greaterThan(9, 11, 20));
 
-function greaterThan (num1, num2, num3) {
-    if (num1 > num2 && num1 > num3) {
-        return num1;
-    } else if (num2 > num1 && num2 > num3) {
-        return num2;
-    } else {
-        return num3;
-    }
-};
+function greaterThan(num1, num2, num3) {
+  if (num1 > num2 && num1 > num3) {
+    return num1;
+  } else if (num2 > num1 && num2 > num3) {
+    return num2;
+  } else {
+    return num3;
+  }
+}
