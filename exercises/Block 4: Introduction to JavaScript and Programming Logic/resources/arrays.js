c55f6576756bac37c173bd8cf39b9873ddb09a98
Luiz Vicentin <luizpavanello@gmail.com>
2020-11-27 15:40:30 -0300

    Block 4 - Day 2 iniciado

---
+++ exercises/Block 4: Introduction to JavaScript and Programming Logic/resources/arrays.js
@@ -0,0 +1,5 @@
+//Arrays
+//https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array
+// Criando um Array:
+// var frutas = ['Maçã', 'Banana'];
+// console.log(frutas.length);
