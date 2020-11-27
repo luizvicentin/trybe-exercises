bba86152f1bcd4ac8911f28094fb946369d6d89b
Luiz Vicentin <luizpavanello@gmail.com>
2020-11-27 19:58:25 -0300

    Exercicio 7

---
+++ exercises/Block 4: Introduction to JavaScript and Programming Logic/Day_02/exercise_07.js
@@ -0,0 +1,13 @@
+//Utilizando for , descubra qual o menor valor contido no array e imprima-o;
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let smallestNumber = 500;
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] < smallestNumber) {
+    smallestNumber = numbers[i];
+  }
+}
+
+console.log(smallestNumber);
