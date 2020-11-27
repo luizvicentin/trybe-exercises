40187a7d33b46caae2ece01097613caf820075f1
Luiz Vicentin <luizpavanello@gmail.com>
2020-11-27 13:44:58 -0300

    Exercise 10

--- exercises/Block 4: Introduction to JavaScript and Programming Logic/Day_01/exercise_10.js
+++ exercises/Block 4: Introduction to JavaScript and Programming Logic/Day_01/exercise_10.js
@@ -1,19 +1,22 @@
-let custoDeProduto = 5;
-let valorBruto = 10;
+//Escreva um programa que se inicie com dois valores em duas variáveis diferentes: o custo de um produto e seu valor de venda.
+//A partir dos valores, calcule quanto de lucro (valor de venda descontado o custo do produto) a empresa terá ao vender mil desses produtos.
+// Sobre o custo do produto, incide um imposto de 20%.
+// Seu programa também deve emitir uma mensagem de erro e encerrar caso algum dos seus valores de entrada seja menor que zero.
+// O lucro de um produto é o resultado da subtração do valor de venda pelo custo do mesmo, sendo que o imposto de 20% também faz parte do valor de custo.
+// valorCustoTotal = valorCusto + impostoSobreOCusto
+// lucro = valorVenda - valorCustoTotal (lucro de um produto)
 
-if (custoDoProduto < 0 || valorDeVenda = 0) {
-    console.log('Entradas inválidas.');
-}   else {
-    let lucroBruto = valorDeVenda - custoDeProduto
+let valorCusto = 100;
+let valorDeVenda = 350;
+let imposto = 0.2;
+let quantidadeVendida = 1000;
 
-    let imposto = custoDoProduto * 0.2;
-    
-    let lucroLiquido = lucroBruto - imposto;
+let valorCustoTotal = valorCusto * (1 + imposto);
 
-    let lucroDeMilProdutos = lucroLiquido * 1000;
-
-    console.log(lucroBruto, lucroLiquido, lucroDeMilProdutos, imposto);
-}
-
-
-
+if (valorCusto < 0 && valorDeVenda < 0 && imposto < 0) {
+  console.log("ERRO: Valores negativos! Encerrando o programa.");
+} else {
+  console.log(
+    "O lucro total é: " + quantidadeVendida * (valorDeVenda - valorCustoTotal)
+  );
+};
