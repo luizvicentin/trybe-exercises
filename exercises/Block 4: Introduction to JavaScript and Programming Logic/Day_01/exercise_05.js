dab14e66c0479e4c3e3def1f9db99de89c50db63
Luiz Vicentin <luizpavanello@gmail.com>
2020-11-27 12:00:03 -0300

    Exercise 5

--- exercises/Block 4: Introduction to JavaScript and Programming Logic/Day_01/exercise_05.js
+++ exercises/Block 4: Introduction to JavaScript and Programming Logic/Day_01/exercise_05.js
@@ -1,13 +1,10 @@
-let ang1 = 60;
-let ang2 = 60;
-let ang3 = 60;
+let ang1 = 100;
+let ang2 = 30;
+let ang3 = 50;
 let sum = ang1 + ang2 + ang3;
 
-
-if ( sum == 180) {
-    isTri = true;
+if ( sum  == 180) {
+    console.log('true');
 } else {
-    isTri = false;
-}
-
-console.log(isTri);
+    console.log('false');
+};
