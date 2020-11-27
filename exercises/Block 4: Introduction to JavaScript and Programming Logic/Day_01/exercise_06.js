99fc960451322f643a3237f1e9614101d45930c7
Luiz Vicentin <luizpavanello@gmail.com>
2020-11-27 12:26:07 -0300

    Exercise 6

--- exercises/Block 4: Introduction to JavaScript and Programming Logic/Day_01/exercise_06.js
+++ exercises/Block 4: Introduction to JavaScript and Programming Logic/Day_01/exercise_06.js
@@ -1,6 +1,32 @@
-// Escreva um programa que receba o nome de uma peça de xadrez e retorne os movimentos que ela faz.
-// Como desafio, faça o programa funcionar tanto se receber o nome de uma peça com letras maiúsculas quanto com letras minúsculas, sem aumentar a quantidade de condicionais.
-// Como dica, você pode pesquisar uma função que faz uma string ficar com todas as letras minúsculas (lower case) .
-// Se a peça passada for inválida, o programa deve retornar uma mensagem de erro.
-// Exemplo: bishop -> diagonals
+let chessPiece = "Knight";
+let chessPieceTLC = chessPiece.toLowerCase();
 
+switch (chessPiece) {
+  case "Pawn":
+    console.log('The Pawn can move forward to the unoccupied square immediately in front of it on the same file, or on its first move it can advance two squares along the same file, provided both squares are unoccupied (white dots in the diagram); or the pawn can capture an opponents piece on a square diagonally in front of it on an adjacent file, by moving to that square (black "x"s). A pawn has two special moves: the en passant capture and promotion.');
+    break;
+
+  case "Knight":
+    console.log('The Knight can move to any of the closest squares that are not on the same rank, file, or diagonal. (Thus the move forms an "L"-shape: two squares vertically and one square horizontally, or two squares horizontally and one square vertically.) The knight is the only piece that can leap over other pieces.');
+    break;
+
+  case 'Bishop':
+    console.log(' The Bishop can move any number of squares diagonally, but cannot leap over other pieces.');
+    break;
+
+  case 'Rock':
+    console.log('The Rock can move any number of squares along a rank or file, but cannot leap over other pieces. Along with the king, a rook is involved during the kings castling move.');
+    break;
+
+  case 'Queen':
+    console.log('The Queen combines the power of a rook and bishop and can move any number of squares along a rank, file, or diagonal, but cannot leap over other pieces.');
+    break;
+
+  case 'King':
+    console.log('The King move one square in any direction. The king also has a special move called castling that involves also moving a rook.');
+    break;
+
+  default:
+    console.log('Value indefine!!')
+    break;
+};
