99df41ce84da7c28e3c5a29da85bb2d44f638e38
Luiz Vicentin <luizpavanello@gmail.com>
2020-11-27 13:30:42 -0300

    Exercise 8

---
+++ exercises/Block 4: Introduction to JavaScript and Programming Logic/Day_01/exercise_08.js
@@ -0,0 +1,10 @@
+let number1 = 5;
+let number2 = 7;
+let number3 = 10;
+let isEven = false;
+
+if (number1 % 2 != 0 || number2 % 2 != 0 || number3 % 2 != 0) {
+  isEven = true;
+}; 
+
+console.log(isEven);
