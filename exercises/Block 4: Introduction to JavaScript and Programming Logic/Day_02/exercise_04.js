0019ec4fa6fab344ed40e7e45c3a47884a23c981
Luiz Vicentin <luizpavanello@gmail.com>
2020-11-27 19:08:58 -0300

    Exercise 4

---
+++ exercises/Block 4: Introduction to JavaScript and Programming Logic/Day_02/exercise_04.js
@@ -0,0 +1,14 @@
+// Com o mesmo código do exercício anterior, caso valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let total = 0;
+
+for ( index = 0; index < numbers.length; index += 1) {
+    total += (numbers[index] / (numbers.length));
+};
+
+if ( total > 20) {
+    console.log ( total.toFixed(2) + ' Valor maior que 20!')
+} else {
+    console.log(total.toFixed(2) + ' Valor menor que 20!');
+}
