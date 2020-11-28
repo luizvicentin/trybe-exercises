76cf03fd1b1791f04b7e783e9d5a3979d9ee9cf0
Luiz Vicentin <luizpavanello@gmail.com>
2020-11-28 18:33:30 -0300

    Exercise 03 - Done

--- exercises/Block 4: Introduction to JavaScript and Programming Logic/Day_03/exercise_03.js
+++ exercises/Block 4: Introduction to JavaScript and Programming Logic/Day_03/exercise_03.js
@@ -1,4 +1,17 @@
 //3. Agora inverta o lado do triângulo.
-//- Atenção! Note que esse exercício é bem mais complexo que o anterior! 
+//- Atenção! Note que esse exercício é bem mais complexo que o anterior!
 //Não basta, aqui, imprimir somente asteriscos. Você precisará de uma lógica para imprimir espaços também.
 
+let number = 5;
+let asterix = "  ";
+let spaces = "  ";
+
+for (let index = 0; index < number; index += 1) {
+  asterix += "*";
+  for (let position = index; position < number; position++) {
+    spaces += "  ";
+  }
+
+  console.log(spaces, asterix);
+  spaces = "  ";
+}
