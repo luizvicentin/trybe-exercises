7141720def767aa8002956c7039058db0643b2c2
Luiz Vicentin <luizpavanello@gmail.com>
2020-11-28 15:45:33 -0300

    Exercise 02 - Done

--- exercises/Block 4: Introduction to JavaScript and Programming Logic/Day_03/exercise_02.js
+++ exercises/Block 4: Introduction to JavaScript and Programming Logic/Day_03/exercise_02.js
@@ -1,3 +1,15 @@
-///2. Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo 
+///2. Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo
 //retângulo com 5 asteriscos de base.
 
+// n = 5
+
+// *
+// **
+// ***
+// ****
+// *****
+
+let n =5;
+
+for (let line = "*"; line.length <= n; line += "*")
+  console.log(line);
