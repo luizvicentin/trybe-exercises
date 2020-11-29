668366408c7c7fbf5fbdf119d86b0fc74136b191
Luiz Vicentin <luizpavanello@gmail.com>
2020-11-29 12:10:05 -0300

    Exercise_04 Update

---
+++ exercises/Block 4: Introduction to JavaScript and Programming Logic/Day_03/exercise_04.2.js
@@ -0,0 +1,21 @@
+let pyramidBase = 7;
+let symbol = '*';
+let lineInput = ' ';
+let pyramidMiddle = (pyramidBase + 1) /  2;
+let left = (pyramidMiddle + 1);
+let right = (pyramidMiddle - 1);
+
+for (indexLines = 0; indexLines < pyramidMiddle; indexLines += 1) {
+  for (indexColumns = 1; indexColumns <= pyramidBase; indexColumns += 1){
+    if (indexColumns > right && indexColumns < left) {
+        lineInput += symbol;      
+    } else {
+      lineInput += '  ';
+    }
+  }
+  console.log(lineInput);
+  lineInput = ' ';
+  right -= 1;
+  left += 1;
+}
+
