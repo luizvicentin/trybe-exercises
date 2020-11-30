b914f912ae1b7d176e5a8f655f51be383a7f4ed0
Luiz Vicentin <luizpavanello@gmail.com>
2020-11-30 15:15:54 -0300

    Exercise 01 - Done

---
+++ exercises/Block 4: Introduction to JavaScript and Programming Logic/Day_04/exercise_01.js
@@ -0,0 +1,20 @@
+operators ( 10 , 5 )
+
+function operators( a , b ) {
+  let sum = a +b;  
+  console.log('The Sum is equal to ' + sum + '.');
+
+  let subtraction = a - b;
+  console.log('The Subtraction is equal to ' + subtraction + '.');
+
+  let multiplication= a * b;
+  console.log('The Multiplication is equal to ' + multiplication + '.');
+  
+  let division = a / b;
+    console.log('The Division is equal to ' + division + '.');
+
+  let module =  a % b;
+    console.log('The Module is equal to ' + module + '.');
+
+  return true;
+}
