5d5b275ccd9c92fed466a631d1878a8540195bd6
Luiz Vicentin <luizpavanello@gmail.com>
2020-11-30 15:41:31 -0300

    Exercise 03 - Done

---
+++ exercises/Block 4: Introduction to JavaScript and Programming Logic/Day_04/exercise_03.js
@@ -0,0 +1,12 @@
+//Compare and return the greater number
+function greaterThan ( a , b , c) {
+  if (a > b && a > c) {
+    return ( a + ' is greater than ' + b + ' and ' + c + '.');
+  } else if (b > a && b > c) {
+    return ( b + ' is greater than ' + a + ' and ' + c + '.' );
+  } else {
+    return ( c + ' is greater than ' + a + ' and ' + b + '.' );
+  }
+}
+
+console.log(greaterThan( 9 , 11 , 20 ))
