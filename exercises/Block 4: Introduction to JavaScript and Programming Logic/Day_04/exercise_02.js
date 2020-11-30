331c310657d8869f515ca4367ba5d7e4c50ebfeb
Luiz Vicentin <luizpavanello@gmail.com>
2020-11-30 15:35:00 -0300

    Exercise 02 - Done

---
+++ exercises/Block 4: Introduction to JavaScript and Programming Logic/Day_04/exercise_02.js
@@ -0,0 +1,10 @@
+//Compare and return the greater number
+function greeaterThan ( a, b ) {
+ if ( a > b ){
+   return a + ' is greater than ' + b + '.';
+ } else {
+   return b + ' is greater than ' + a + '.';
+ }
+};
+
+console.log(greeaterThan ( 73, 37 ));
