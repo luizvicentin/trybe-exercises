11d70472ca9cb8fe958ff1bae991ef368c151009
Luiz Vicentin <luizpavanello@gmail.com>
2020-12-03 18:07:58 -0300

    Exercise 01

--- exercises/Block 4: Introduction to JavaScript and Programming Logic/Day_01/exercise_01.js
+++ exercises/Block 4: Introduction to JavaScript and Programming Logic/Day_01/exercise_01.js
@@ -4,16 +4,16 @@ let a = 5;
 let b = 10;
 
 let soma = a + b;
-console.log('A soma de x e y é igual a ' + soma + '.');
+console.log('A soma de a e b é igual a ' + soma + '.');
 
 let subt =  a - b;
-console.log('A subtração de x e y é igual a '  + subt + '.');
+console.log('A subtração de a e b é igual a '  + subt + '.');
 
 let multi = a * b;
-console.log('A multiplicação de x e y é igual a ' + multi + '.');
+console.log('A multiplicação de a e b é igual a ' + multi + '.');
 
 let div = a / b;
-console.log('A divisão entre x e y é igual a ' + div + '.');
+console.log('A divisão entre a e b é igual a ' + div + '.');
 
 let mod = a % b;
-console.log('O módulo entre x e y dá o resto igual a ' + mod + '.');
+console.log('O módulo entre a e b dá o resto igual a ' + mod + '.');
