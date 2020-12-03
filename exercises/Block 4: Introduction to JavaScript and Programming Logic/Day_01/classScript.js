48519938cb4b1962508f071b77178654ec72c669
Luiz Vicentin <luizpavanello@gmail.com>
2020-12-03 15:26:48 -0300

    Updates

---
+++ exercises/Block 4: Introduction to JavaScript and Programming Logic/Day_01/classScript.js
@@ -0,0 +1,8 @@
+let age = 35;
+let name = 'Luiz';
+
+if ( age < 18 ) {
+    console.log( `${name}, voce nao pode dirigir.` ); 
+    } else {
+    console.log( `${name}, voce pode dirigir.` ) 
+};
---
+++ exercises/Block 4: Introduction to JavaScript and Programming Logic/Day_01/examples.js
@@ -0,0 +1,43 @@
+// const name = 'Luiz'
+// const birthCity = 'Vinhedo';
+// let birthYear = 1985;
+
+// console.log( name, birthCity, birthYear );
+// console.log( birthCity );
+// console.log( birthYear );
+
+// birthYear = 2020;
+// console.log (birthYear);
+
+// birthCity = 'SP'; //Assignment to constant variable, can't change. 
+// console.log (birthCity);
+
+// → typeof
+// let movie = 'Avengers'; //string literal
+// let score = 10; //number literal
+// let isValid = true; //boolean
+// let name; //undefined
+// let color = null; // 
+
+// let patientId = 50;
+// let isEnrolled = true;
+// const patientInfo = {
+//     firstNsme: 'Ana',
+//     lastName: 'Santos',
+// };
+
+// const patientEmail = 'ana@email.com';
+
+// console.log(typeof patientAge);
+
+// let base = 5;
+// let altura = 8;
+// let area  = base * altura;
+// let perimetro = (base + altura) * 2;
+
+// console.log(area);
+// console.log(perimetro);
+
+
+
+
