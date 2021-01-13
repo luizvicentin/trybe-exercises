04d94f98f89a766c5790ba51259590f42f033be8
Luiz Vicentin <luizpavanello@gmail.com>
2021-01-13 14:54:07 -0300

    Day_01

---
+++ exercises/Block 7 - Introduction to JavaScript ES6 and Unit Tests/Day_01_JavaScript ES6 - let, const, arrow functions e template literals/exercise1.js
@@ -0,0 +1,33 @@
+//Parte 1
+// Faça as modificações necessárias na função para que o seu comportamento respeite o escopo no qual cada variável foi declarada.
+// Modifique a estrutura da função para que ela seja uma arrow function .
+// Modifique as concatenações para template literals .
+// Copie o código abaixo.
+const testingScope = escopo => { 
+    if (escopo === true) { 
+      let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+      ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+      console.log(ifScope);
+    } else {
+      let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+      console.log(elseScope);
+    }
+    // console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+  }
+
+  testingScope(true);
+
+//Parte 2
+//Copie o código abaixo e faça uma função que retorne o array oddsAndEvens em ordem crescente.
+// Utilize template literals para que a chamada console.log(oddsAndEvens); retorne "Os números 2,3,4,7,10,13 se encontram ordenados de forma crescente!".
+// Bônus (opcional): tente fazer o mesmo exercício utilizando o método array.sort() . Spoiler: É possível realizar uma função que ordene qualquer array de números.
+// Copie o código abaixo.
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+console.log(oddsAndEvens);
+
+const sortOddsAndEvens = array => array.sort((a, b) => a - b);
+
+console.log(`Os números ${sortOddsAndEvens([13, 3, 4, 10, 7, 2])} se encontram ordenados de forma crescente!`);
+// Seu código aqui.
+
---
+++ exercises/Block 7 - Introduction to JavaScript ES6 and Unit Tests/Day_01_JavaScript ES6 - let, const, arrow functions e template literals/exercise2.js
@@ -0,0 +1,63 @@
+// Parte 1
+// Crie uma função que receba um número e retorne seu fatorial.
+// Na matemática, o fatorial de um número não negativo N , com a notação N! , é o produto de todos os inteiros menores ou iguais a N . Exemplo: 4! = 4 * 3 * 2 * 1 = 24.
+const fatorial = n => n <= 1 ? 1 : n * fatorial(n-1);
+console.log(fatorial(5));
+
+// Parte 2
+//Crie uma função que receba uma frase e retorne qual a maior palavra.
+const longestWord = string => {
+
+    const arrayString = string.split(' ');
+    let longestWord = '';
+
+    for (let index = 0; index < arrayString.length; index += 1) {
+        if (arrayString[index].length > longestWord.length) {
+            longestWord = arrayString[index];
+        }
+    }
+    return longestWord;
+};
+
+console.log(longestWord('Antônio foi no banheiro e não sabemos o que aconteceu'));
+
+// Parte 3
+// 
+// window.onload = function () {
+//     let clickCount = 0;
+//     const count = document.getElementById('click-count');
+//     const button = document.getElementById('click-button');
+//     button.addEventListener('click', () => count.innerText = clickCount += 1);
+// }
+
+// Parte 4 - stringPhrase.replace() function learned through: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/replace
+
+const changeString = string => {
+    let stringPhrase = 'Tryber x aqui!';
+
+    for (let index = 0; index < stringPhrase.length; index += 1) {
+        if (stringPhrase[index] === 'x') {
+            return stringPhrase.replace(/x/i, string);
+        }
+    }
+
+    return stringPhrase;
+};
+
+
+let stringPhrase = changeString('Luiz');
+console.log(stringPhrase);
+
+let mySkills = ['HTML', 'CSS', 'FlexBox', 'JavaScript', 'Soft-Skills'];
+const finalString = string => {
+    mySkills = mySkills.sort();
+    return `${string} Minhas cinco principais habilidades são:
+    ${mySkills[0]};
+    ${mySkills[1]};
+    ${mySkills[2]};
+    ${mySkills[3]};
+    ${mySkills[4]};
+    ... #goTrybe`
+};
+
+console.log(finalString(stringPhrase));
