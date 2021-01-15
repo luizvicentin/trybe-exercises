4a9fbb17ee3ffdc46afce1d802214b93c5c845a0
Luiz Vicentin <luizpavanello@gmail.com>
2021-01-15 09:38:24 -0300

    Day_03 → Exercise 03

---
+++ exercises/Block 7 - Introduction to JavaScript ES6 and Unit Tests/Day_03_JavaScript Unit Testing/exercise03.js
@@ -0,0 +1,31 @@
+const assert = require('assert');
+
+function myRemoveWithoutCopy(arr, item) {
+  for (let i = 0, len = arr.length; i < len; i += 1) {
+    if (arr[i] === item) {
+      arr.splice(i, 1);
+      i -= 1;
+      len -= 1;
+    }
+  }
+
+  return arr;
+}
+
+// 1. A função myRemoveWithoutCopy(arr, item) recebe um array arr e retorna o próprio array sem o elemento item caso ele exista no array
+// 2. Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 3) retorna o array esperado
+// 3. Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
+// 4. Faça uma chamada para a função myRemoveWithoutCopy e verifique se o array passado por parâmetro sofreu alterações
+// 5. Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 5) retorna o array esperado
+
+// implemente seus testes aqui
+
+const myList = [1, 2, 3, 4];
+
+assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2, 4]);
+assert.notDeepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2, 3, 4]);
+
+myRemoveWithoutCopy(myList, 1);
+assert.strictEqual(myList.length, 3);
+
+assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 5), [1, 2, 3, 4]);
