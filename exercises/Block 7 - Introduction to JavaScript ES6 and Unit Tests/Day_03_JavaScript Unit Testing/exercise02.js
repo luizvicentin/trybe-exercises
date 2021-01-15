59f5909e66b8dc91dccb7fe578d91d2dea1b18f5
Luiz Vicentin <luizpavanello@gmail.com>
2021-01-15 09:36:51 -0300

    Day_03 → Exercise 02

---
+++ exercises/Block 7 - Introduction to JavaScript ES6 and Unit Tests/Day_03_JavaScript Unit Testing/exercise02.js
@@ -0,0 +1,29 @@
+const assert = require('assert');
+
+function myRemove(arr, item) {
+  let newArr = [];
+  for (let index = 0; index < arr.length; index += 1) {
+    if (item !== arr[index]) {
+      newArr.push(arr[index]);
+    }
+  }
+  return newArr;
+}
+
+// 1. A função myRemove(arr, item) recebe um array arr e retorna uma cópia desse array sem o elemento item caso ele exista no array
+// 2. Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado
+// 3. Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
+// 4. Verifique se o array passado por parâmetro não sofreu alterações
+// 5. Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado
+
+// implemente seus testes aqui
+
+assert.strictEqual(typeof myRemove, 'function');
+assert.deepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 4]);
+assert.notDeepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 3, 4]);
+
+const myList = [5, 6, 7, 8];
+myRemove(myList, 5);
+assert.deepStrictEqual(myList, [5, 6, 7, 8]);
+
+assert.deepStrictEqual(myRemove([1, 2, 3, 4], 5), [1, 2, 3, 4]);
