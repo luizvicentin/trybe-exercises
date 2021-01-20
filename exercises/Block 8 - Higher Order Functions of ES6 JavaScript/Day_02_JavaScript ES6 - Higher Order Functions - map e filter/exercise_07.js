62cdff989a65c72c53d18144f37ddf74b53a0a1b
Luiz Vicentin <luizpavanello@gmail.com>
2021-01-20 10:09:24 -0300

    Day_02_Exercise_07.js

--- exercises/Block 8 - Higher Order Functions of ES6 JavaScript/Day_02_JavaScript ES6 - Higher Order Functions - map e filter/exercise_07.js
+++ exercises/Block 8 - Higher Order Functions of ES6 JavaScript/Day_02_JavaScript ES6 - Higher Order Functions - map e filter/exercise_07.js
@@ -69,9 +69,9 @@ const expectedResult = 'O Senhor dos Anéis';
 
 function authorWith3DotsOnName() {
   // escreva seu código aqui
-  return books.find((book) => {
-    return book.author.name.search(/.\.\s.\.\s.\.\s/) >= 0;
-  }).name;
+  return books.find(book => (
+    book.author.name.split(' ').filter(word => word.endsWith('.')).length === 3
+  )).name;
 };
 
 assert.deepStrictEqual(authorWith3DotsOnName(), expectedResult);
