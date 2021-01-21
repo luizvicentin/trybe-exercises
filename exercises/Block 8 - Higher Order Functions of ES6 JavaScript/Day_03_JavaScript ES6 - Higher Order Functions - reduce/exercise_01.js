d1422be3bc287038257e7ffa9b7008df3410c071
Luiz Vicentin <luizpavanello@gmail.com>
2021-01-21 11:25:18 -0300

    Day_03_Exercise_01.js

---
+++ exercises/Block 8 - Higher Order Functions of ES6 JavaScript/Day_03_JavaScript ES6 - Higher Order Functions - reduce/exercise_01.js
@@ -0,0 +1,20 @@
+// 1. Dada uma matriz de matrizes, transforme em uma única matriz.
+
+
+const assert = require('assert');
+
+const arrays = [
+    ["1", "2", "3"],
+    [true],
+    [4, 5, 6]
+];
+
+
+function flatten() {
+  // escreva seu código aqui
+  return arrays.reduce((result, currentValue) => {
+    return result.concat(currentValue);
+  });
+}
+
+assert.deepStrictEqual(flatten(), ["1", "2", "3", true, 4, 5, 6]);
