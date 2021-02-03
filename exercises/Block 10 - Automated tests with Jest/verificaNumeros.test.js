efc606c4e36cb4685c56fe0196371fdf4b4c04f6
Luiz Vicentin <luizpavanello@gmail.com>
2021-02-03 19:12:02 -0300

    Block_10 → Jest IT → verificaNumeros

--- exercises/Block 10 - Automated tests with Jest/verificaNumeros.test.js
+++ exercises/Block 10 - Automated tests with Jest/verificaNumeros.test.js
@@ -5,3 +5,15 @@ describe('Requisito 1', () => {
       expect(true).toEqual(numeros([1, 2, 3, 4, 5]));
     });
 });
+
+describe('Requisito 2', () => {
+  it(`A funcao recebe [1, 2, '3' , 4, 5] e retorna false`, () => {
+    expect(false).toEqual(numeros([1, 2, '3' , 4, 5]));
+  });
+});
+
+describe('Requisito 3', () => {
+  it(`A funcao recebe [ ' ' ] e retorna false`, () => {
+    expect(false).toEqual(numeros([ ' ' ]));
+  });
+});
