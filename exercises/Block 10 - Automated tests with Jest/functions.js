12273251eb5b8e9d3f6193ff1ea5777404480740
Luiz Vicentin <luizpavanello@gmail.com>
2021-02-03 22:41:57 -0300

    Block_10 → Jest IT → functions.js

--- exercises/Block 10 - Automated tests with Jest/functions.js
+++ exercises/Block 10 - Automated tests with Jest/functions.js
@@ -1,8 +1,21 @@
 const functions = {
-    add: (num1, num2) => num1 + num2
+    add: (num1, num2) => num1 + num2,
     // add: function(num1, num2) {
     //     return num1 + num2;
     // };
-}
+    isNull: () => null,
+    checkValue: x => x,
+    createUser: () => {
+      const user = { firstName: 'Luiz' };
+      user['lastName'] = 'Vicentin';
+      return user;
+    },
+    // fetchUser: () =>
+    // axios → should install npm i axios
+    //   .get('https://jsonplaceholder.typicode.com/users/1')
+    //   .then(res => res.data)
+    //   .catch(err => 'error')
+};
+
 // const sum = (a, b) => a + b;
 module.exports = functions;
--- exercises/Block 10 - Automated tests with Jest/functions.test.js
+++ exercises/Block 10 - Automated tests with Jest/functions.test.js
@@ -1,5 +1,71 @@
 const functions = require("./functions");
 
+// toBe
 test( 'Adds 2 + 2 to equal 4', () => {
-    expect(functions.add(2, 2)).toBe(4);
+  expect(functions.add(2, 2)).toBe(4);
 });
+
+// not
+test( 'Adds 2 + 2 to Not equal 5', () => {
+  expect(functions.add(2, 2)).not.toBe(5);
+});
+
+// CHECK FOR TRUTHY & FALSY VALUES
+// toBeNull matches only null
+// toBeUndefined matches only undefined
+// toBeDefined is the opposite of toBeUndefined
+// toBeTruthy matches anything that an if statement treats as true
+// toBeFalsy matches anything that an if statement treats as false
+
+// toBeNull
+test('Should be null', () => {
+  expect(functions.isNull()).toBeNull();
+});
+
+  // toBeFalsy
+test('Should be falsy', () => {
+  expect(functions.checkValue(undefined)).toBeFalsy();
+});
+
+  //toEqual → Object or Array
+test('User should be Luiz Vicentin object', () => {
+    expect(functions.createUser()).toEqual({
+        firstName: 'Luiz',
+        lastName: 'Vicentin',
+    });
+});
+
+// Less than and greater than
+test('Should be under 1700', () => {
+    const load1 = 800;
+    const load2 = 900;
+    expect(load1 + load2).toBeLessThanOrEqual(1700);
+});
+
+// Regex
+test('There is no I in team', () => {
+    expect('team').not.toMatch(/I/i);
+  });
+  
+  // Arrays
+  test('Admin should be in usernames', () => {
+    usernames = ['john', 'karen', 'admin'];
+    expect(usernames).toContain('admin');
+  });
+  
+//   // Working with async data
+  
+//   // Promise
+//   // test('User fetched name should be Leanne Graham', () => {
+//   //   expect.assertions(1);
+//   //   return functions.fetchUser().then(data => {
+//   //     expect(data.name).toEqual('Leanne Graham');
+//   //   });
+//   // });
+  
+//   // Async Await
+//   test('User fetched name should be Leanne Graham', async () => {
+//     expect.assertions(1);
+//     const data = await functions.fetchUser();
+//     expect(data.name).toEqual('Leanne Graham');
+//   });
